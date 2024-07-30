3becf8768273f8156994e753481d9e3fae01e3ce
Anatoli Gavrash <gaver5@yandex.ru>
2024-07-30 18:39:53 +0300

    Change connect route and app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css';
+import {Outlet} from 'react-router-dom';
+import {Box} from '@mui/material';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <>
-    </>
+    <Box className={styles.app}
+      height="100%"
+      display="flex"
+      justifyContent="center"
+      p={5}
+      py={10}
+    >
+      <Outlet />
+    </Box>
   );
 }
 
