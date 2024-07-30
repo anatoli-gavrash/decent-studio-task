03cfd7279a0d1520015eb1f5d54d244a7faba1b7
Anatoli Gavrash <gaver5@yandex.ru>
2024-07-30 16:20:17 +0300

    Add list component

---
+++ src/components/list/list.jsx
@@ -0,0 +1,35 @@
+import {useEffect, useState} from 'react';
+import {Container, Grid} from '@mui/material';
+import {Link} from 'react-router-dom';
+import {fetchData} from '../../utils';
+import styles from './list.module.css';
+
+const List = () => {
+  const [countries, setCountries] = useState(null);
+
+  useEffect(() => {
+    if (!countries) {
+      fetchData('https://restcountries.com/v3.1/all?fields=name,flag,flags')
+        .then((data) => setCountries(data[1]));
+    }
+  }, []);
+
+  return (
+    <Container className={styles.list}>
+      <Grid justifyContent={'center'} className={styles.grid} container spacing={2}>
+        {countries && countries.map((country) => {
+          return (
+            <Grid className={styles.item} item xs={12} sm={10} md={6} lg={4}>
+              <Link className={styles.link} to={`/${country.name.common}`}>
+                <img className={styles.flag} src={country.flags.png} alt={country.flags.alt || ''} />
+                <h3 className={styles.title}>{country.name.common}</h3>
+              </Link>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Container>
+  );
+};
+
+export default List;
